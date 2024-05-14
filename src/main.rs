6f0e33b46db3a8ca362ff27212e25f24960811cb
wasuken <wevorence@gmail.com>
2024-05-14 09:34:59 +0900

    色とか配置をランダムにしてる途中

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,32 @@ use std::fs::File;
 use std::io::BufReader;
 use std::thread;
 use std::time::Duration;
-use terminal::{Action, Clear, Color};
+use terminal::{Action, Clear, Color, Retrieved, Value};
+
+fn random_color() -> Color {
+    let mut rng = rand::thread_rng();
+    match rng.gen_range(0..20) {
+        0 => Color::Reset,
+        1 => Color::Black,
+        2 => Color::DarkGrey,
+        3 => Color::Red,
+        4 => Color::DarkRed,
+        5 => Color::Green,
+        6 => Color::DarkGreen,
+        7 => Color::Yellow,
+        8 => Color::DarkYellow,
+        9 => Color::Blue,
+        10 => Color::DarkBlue,
+        11 => Color::Magenta,
+        12 => Color::DarkMagenta,
+        13 => Color::Cyan,
+        14 => Color::DarkCyan,
+        15 => Color::White,
+        16 => Color::Grey,
+        17 => Color::Rgb(rng.gen(), rng.gen(), rng.gen()),
+        _ => Color::AnsiValue(rng.gen()),
+    }
+}
 
 fn main() {
     // JSONファイルを読み込む
@@ -14,22 +39,42 @@ fn main() {
 
     // ターミナルのインスタンスを作成
     let terminal = terminal::stdout();
+    let mut max_width = 0;
+    let mut max_height = 0;
+
+    if let Retrieved::TerminalSize(x, y) = terminal
+        .get(Value::TerminalSize)
+        .expect("error: terminal size.")
+    {
+        max_width = x;
+        max_height = y;
+    }
 
     // 無限ループ
     loop {
-        // ランダムな文を選択
-        let index = rand::thread_rng().gen_range(0..data.len());
-        let text = &data[index];
+        // 画面初期化
         terminal
             .batch(Action::ClearTerminal(Clear::All))
             .expect("error: clear");
+        // ランダムな文を選択
+        let index = rand::thread_rng().gen_range(0..data.len());
+        let text = &data[index];
+        let x = rand::thread_rng().gen_range(0..max_width);
+        // 20をtext.len()にするとおちる。
+        let mut trun_by_text_len_width = max_height - 20;
+        if trun_by_text_len_width < 0 {
+            trun_by_text_len_width = 0;
+        }
+        let y = rand::thread_rng().gen_range(0..trun_by_text_len_width);
+        // ランダムな位置を指定
         terminal
-            .batch(Action::MoveCursorTo(0, 0))
-            .expect("error: clear");
+            .batch(Action::MoveCursorTo(x, y))
+            .expect("error: cursor to");
+        // ランダムな色指定
         terminal
-            .batch(Action::SetForegroundColor(Color::Green))
+            .batch(Action::SetForegroundColor(random_color()))
             .expect("error: set foreground color");
-        print!("{}", &text);
+        print!("{} ({}:{})", &text, x, y);
         terminal
             .batch(Action::ResetColor)
             .expect("error: reset color");
